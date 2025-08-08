75f55df8f8c478b0a8bcfd1ca68831085f4e37d6
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-08-08 18:04:16 +0600

    task1 done

---
+++ function.js
@@ -0,0 +1,39 @@
+/*
+# Simple function Related Practice Tasks
+
+### Task-1  
+Take four parameters. Multiply the four numbers and then return the result 
+*/
+
+function mul(num1, num2, num3, num4) {
+  return num1 * num2 * num3 * num4;
+}
+
+const mult = mul(2, 3, 4, 5);
+console.log(mult);
+
+/*
+### Task-2
+Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+*/
+
+
+
+
+
+/*
+### Task-3
+Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+
+
+*/
+
+/*
+### Task-4  
+Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+*/
+
+/*
+### Task-5 
+Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return `Even`. If odd return `Odd`
+*/
