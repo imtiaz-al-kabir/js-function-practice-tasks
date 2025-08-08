fb502e44787c70f1952b26ceed352d9c1ab55d59
Imtiaz AL Kabir <imtiazalkabir@gmail.com>
2025-08-08 18:41:53 +0600

    task 5 done

--- function.js
+++ function.js
@@ -73,4 +73,11 @@ console.log(counts);
 Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return `Even`. If odd return `Odd`
 */
 
-function odd_even() {}
+function odd_even(nums) {
+  if (nums % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+}
+console.log(odd_even(6));
